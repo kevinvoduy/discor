965dfb2d9ff2b1432fa99e79f8cca287c59aef5a
Kevin V <kevinvoduy218@gmail.com>
2018-04-19 20:39:42 -0700

    [signup] now redirects to home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Landing from './components/landing';
+import Home from './components/home';
 
 const App = () => (
-  <div>
-    <Landing />
-  </div>
+  <Switch>
+    <Route path='/home' component={Home} />
+    <Route path='/' component={Landing} />
+  </Switch>
 );
 
 export default App;
