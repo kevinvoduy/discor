f3f90d994587354fab5a6776099650a99b61cc11
Kevin V <kevinvoduy218@gmail.com>
2018-06-18 17:08:38 -0700

    [inbox] css template done

--- client/src/components/home/content_switcher/inbox/index.jsx
+++ client/src/components/home/content_switcher/inbox/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-import Chat from './chat';
 import ErrorBoundary from '../../../globals/errorHandler';
 import './inbox.sass';
 
@@ -19,11 +18,92 @@ class Inbox extends React.Component {
   }
 
   render() {
-    console.log('state', this.state);
+    const levy = 'https://frostsnow.com/uploads/biography/2017/11/16/levy-tran.gif';
+
     return (
       <div className="inbox">
         <ErrorBoundary>
-          <Chat socket={this.state.socket} />
+          <div className="header">
+            <div className="inbox__heading">
+              <h3>Inbox (2)</h3>
+            </div>
+            <div className="new__message">
+              <button>+ New Message</button>
+            </div>
+          </div>
+
+          <div className="messages">
+            <div className="message">
+              <div className="icon">
+                <img src={levy} alt="ico" />
+              </div>
+              <div className="name">
+                <p><strong>Kevin Vo</strong></p>
+                <p style={{ color: 'Tomato' }}>Unread</p>
+              </div>
+              <div className="details">
+                <p><strong>We need the lease documents</strong></p>
+                <p>Hi there Justin, we would like to get our lease...</p>
+              </div>
+              <div className="meta">
+                <p>20 min ago</p>
+              </div>
+              <div className="show__more">
+                <img src="" alt="ico" />
+              </div>
+            </div>
+
+            <div className="message">
+              <div className="icon">
+                <img src={levy} alt="ico" />
+              </div>
+              <div className="name">
+                <p><strong>Kevin Vo</strong></p>
+                <p style={{ color: 'lawngreen' }}>Replied</p>
+              </div>
+              <div className="details">
+                <p><strong>We need the lease documents</strong></p>
+                <p>Hi there Justin, we would like to get our lease...</p>
+              </div>
+              <div className="meta">
+                <p>20 min ago</p>
+              </div>
+              <div className="show__more">
+                <img src="" alt="ico" />
+              </div>
+            </div>
+
+            <div className="message">
+              <div className="icon">
+                <img src={levy} alt="ico" />
+              </div>
+              <div className="name">
+                <p><strong>Kevin Vo</strong></p>
+                <p style={{ color: 'gray' }}>Read</p>
+              </div>
+              <div className="details">
+                <p><strong>We need the lease documents</strong></p>
+                <p>Hi there Justin, we would like to get our lease...</p>
+              </div>
+              <div className="meta">
+                <p>20 min ago</p>
+              </div>
+              <div className="show__more">
+                <img src="" alt="ico" />
+              </div>
+            </div>
+          </div>
+
+          <div className="pages">
+            <div className="page__text">
+              <p>Showing 0-6 out of 24</p>
+            </div>
+            <div className="pagenation">
+              <a href="/home">1</a>
+              <a href="/home">2</a>
+              <a href="/home">3</a>
+            </div>
+          </div>
         </ErrorBoundary>
       </div>
     );
