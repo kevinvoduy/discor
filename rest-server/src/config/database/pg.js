72644ea01180c1593fa5a87a82a345a57eca6e95
Kevin V <kevinvoduy218@gmail.com>
2018-07-01 19:51:12 -0700

    [psql] reverting back to old config

--- rest-server/src/config/database/pg.js
+++ rest-server/src/config/database/pg.js
@@ -9,11 +9,11 @@ require('dotenv').config();
 
 const config = {
   user: process.env.NODE_ENV === 'production' ? process.env.AWS_USER : process.env.LOCAL_USER,
-  serverName: process.env.NODE_ENV === 'production' ? process.env.AWS_HOST : process.env.LOCAL_HOST,
-  databaseName: process.env.NODE_ENV === 'production' ? process.env.AWS_DATABASE : 'postgres',
+  host: process.env.NODE_ENV === 'production' ? process.env.AWS_HOST : process.env.LOCAL_HOST,
+  database: process.env.NODE_ENV === 'production' ? process.env.AWS_DATABASE : 'postgres',
   password: process.env.NODE_ENV === 'production' ? process.env.AWS_PASSWORD : process.env.LOCAL_PASSWORD,
-  portNumber: process.env.NODE_ENV === 'production' ? process.env.AWS_PORT : process.env.LOCAL_PORT,
-  maxConnection: 10
+  port: process.env.NODE_ENV === 'production' ? process.env.AWS_PORT : process.env.LOCAL_PORT,
+  max: 10
 };
 
 const db = new Pool(config);
