f2dee8b264bcb74b46a3eea8092e62f3241815d1
Kevin V <kevinvoduy218@gmail.com>
2018-07-06 23:26:07 -0700

    [s3] fixed credentials error

--- posts-server/src/components/upload/uploadRouter.js
+++ posts-server/src/components/upload/uploadRouter.js
@@ -4,14 +4,13 @@ import AWS from 'aws-sdk';
 
 require('dotenv').config();
 
-const s3 = new AWS.S3();
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  region: 'us-west-1',
 });
-console.log('AWS', AWS.config.credentials);
 
+const s3 = new AWS.S3();
 const router = express.Router();
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -19,13 +18,12 @@ const upload = multer({
 });
 
 router.post('/photo', upload.single('photo'), (req, res) => {
-  s3.upload({
+  s3.putObject({
     Bucket: 'discor-photos',
     Key: req.file.originalname,
     Body: req.file.buffer,
     ACL: 'public-read',
   }, err => {
-    console.log(err);
     if (err) return res.status(400).send(err);
     res.send('File uploaded to S3');
   });
